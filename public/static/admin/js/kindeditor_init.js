2cbe79a77f61cbe7463bb020a58b92937deeb4b7
worklz <worklz@163.com>
2019-11-05 17:29:33 +0800

    add kindeditor

---
+++ public/static/admin/js/kindeditor_init.js
@@ -0,0 +1,43 @@
+;(function (win) {
+    win.kindEditorInit = function () {
+        layui.use(['layer', 'jquery'], function () {
+            $ = layui.jquery;
+
+            //获取容器
+            let editorEl = $("#kindeditor_container");
+            //获取外部数据
+            let data = editorEl.data('data');
+            //配置编辑器的工具栏
+            let items = [
+                'source', '|', 'undo', 'redo', '|', 'preview', 'code', '|',
+                'justifyleft', 'justifycenter', 'justifyright', 'justifyfull',
+                'insertorderedlist', 'insertunorderedlist', 'formatblock', 'fontname', 'fontsize', '|',
+                'forecolor', 'hilitecolor', 'bold', 'italic', 'underline', '|',
+                'image', 'insertfile', 'table', 'hr', 'link'
+            ];
+            if (isMobile()) {
+                items = [
+                    'undo', 'redo', '|', 'preview', 'code', '|',
+                    'insertorderedlist', 'insertunorderedlist', 'formatblock', '|',
+                    'image', 'insertfile', 'table', 'hr', 'link'
+                ];
+            }
+            //创建编辑器
+            KindEditor.ready(function (K) {
+                window.editor = K.create('#kindeditor_container', {
+                    items: items,
+                    resizeType: 1,
+                    pasteType: 1,
+                    width: '100%',
+                    minHeight: 300,
+                    syncType: '',
+                    afterChange: function () {
+                        let that = this;
+                        editorEl.html(that.html());
+                    },
+                    allowFileManager: true
+                });
+            });
+        });
+    };
+})(window);
